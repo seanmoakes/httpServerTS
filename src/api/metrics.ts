2be4df86842faa1acf442621feb23d1e43ef9f57
Sean Moakes <mail@seanmoakes.com>
2025-08-26 17:42:15 +0100

    admin

--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -11,5 +11,12 @@ export function middlewareMetricsInc(
 }
 
 export async function handlerMetrics(_: Request, res: Response) {
-  res.send(`Hits: ${config.fileServerHits}`);
+  res.set("Content-Type", "text/html; charset=utf-8");
+  res.send(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${config.fileServerHits} times!</p>
+  </body>
+</html>`
+  );
 }
